6c0c6a57b70693508703030ea0cba9932ccb14b8
feczkob <botond.feczko@tngtech.com>
2024-08-14 19:02:34 +0200

    remove vararg ctrs

--- src/test/kotlin/com/constructor/quasi/KotlinColorStockTest.kt
+++ src/test/kotlin/com/constructor/quasi/KotlinColorStockTest.kt
@@ -18,17 +18,6 @@ class KotlinColorStockTest {
     }
 
     @Test
-    fun `secondary ctr works`() {
-        val colors = ColorStock(
-            Color.RED to 10,
-            Color.WHITE to 20,
-            Color.GREEN to 30
-        )
-
-        assert(colors.colors.size == 3)
-    }
-
-    @Test
     fun `quasi primary ctr works`() {
         val colors = ColorStock(
             mapOf(
@@ -36,17 +25,6 @@ class KotlinColorStockTest {
                 "WHITE" to 20,
                 "GREEN" to 30
             )
-        )
-
-        assert(colors.colors.size == 3)
-    }
-
-    @Test
-    fun `quasi secondary ctr works`() {
-        val colors = ColorStock(
-            "RED" to 10,
-            "WHITE" to 20,
-            "GREEN" to 30
         )
 
         assert(colors.colors.size == 3)
