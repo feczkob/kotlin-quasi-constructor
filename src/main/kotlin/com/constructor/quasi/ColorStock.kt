3792d007337fc458697d5317fd40a1d7f4b341d9
feczkob <feczkob@gmail.com>
2024-08-14 18:59:48 +0200

    add vararg ctrs and README.md

--- src/main/kotlin/com/constructor/quasi/ColorStock.kt
+++ src/main/kotlin/com/constructor/quasi/ColorStock.kt
@@ -1,25 +1,32 @@
 package com.constructor.quasi
 
-class ColorStock(private val values: Map<Color, Amount>) {
+import com.constructor.quasi.Color.valueOf
+
+class ColorStock constructor(private val values: Map<Color, Int>) {
 
     val colors: Set<Color>
         get() = values.keys
 
+    fun amountFor(color: Color) = values[color] ?: error("Color not found")
+
     // * http://www.angelikalanger.com/GenericsFAQ/FAQSections/ProgrammingIdioms.html#FAQ300
     @SafeVarargs
-    constructor(vararg varargs: Pair<Color, Amount>) : this(mapOf(*varargs))
+    constructor(vararg varargs: Pair<Color, Int>) : this(mapOf(*varargs))
+
+    // Overload resolution ambiguity.
+//    constructor(values: Map<String, Int>) : this(values.mapKeys { valueOf(it.key) })
 
     companion object {
-        operator fun invoke(values: Map<String, Amount>) = ColorStock(values.mapKeys { Color.valueOf(it.key) })
+        operator fun invoke(values: Map<String, Int>) = ColorStock(values.mapKeys { valueOf(it.key) })
         @SafeVarargs
-        operator fun invoke(vararg varargs: Pair<String, Amount>) = ColorStock(mapOf(*varargs))
+        operator fun invoke(vararg varargs: Pair<String, Int>) = ColorStock(mapOf(*varargs))
+
+        fun fromStringMap(values: Map<String, Int>) = ColorStock(values.mapKeys { valueOf(it.key) })
     }
 }
 
-class Amount(private val quantity: Int)
-
 enum class Color {
     RED,
-    BLUE,
-    YELLOW
+    WHITE,
+    GREEN;
 }
